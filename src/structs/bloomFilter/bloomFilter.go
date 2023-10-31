cbe2afe6e1112525c9c7373bfce860eb2f00a046
Cofi420 <filip.vidakovic.04@gmail.com>
2023-10-31 17:02:34 +0100

    Add constructor

--- src/structs/bloomFilter/bloomFilter.go
+++ src/structs/bloomFilter/bloomFilter.go
@@ -5,3 +5,23 @@ type BloomFilter struct {
 	k, m     uint
 	hashFunc []HashWithSeed
 }
+func newBf(n int, p float64) BloomFilter { // n je ocekivan br elem, p vjerovatnoca
+	m := CalculateM(n, p)
+	k := CalculateK(n, m)
+
+	return BloomFilter{
+		bitset:   make([]byte, m),
+		k:        k,
+		m:        m,
+		hashFunc: CreateHashFunctions(k),
+	}
+}
+
+func CalculateM(expectedElements int, falsePositiveRate float64) uint {
+	return uint(math.Ceil(float64(expectedElements) * math.Abs(math.Log(falsePositiveRate)) / math.Pow(math.Log(2), float64(2))))
+}
+
+func CalculateK(expectedElements int, m uint) uint {
+	return uint(math.Ceil((float64(m) / float64(expectedElements)) * math.Log(2)))
+}
+
