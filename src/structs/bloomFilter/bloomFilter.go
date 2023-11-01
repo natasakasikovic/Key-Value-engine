2c5608423881ab5fbffdeee5d3ae87e8e39038f1
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2023-11-01 23:26:36 +0100

    fix serialization/deserialization bugs

--- src/structs/bloomFilter/bloomFilter.go
+++ src/structs/bloomFilter/bloomFilter.go
@@ -50,32 +50,46 @@ func (b *BloomFilter) Insert(s string) {
 	}
 }
 
-func Serialize(b *BloomFilter) []byte {
-	var size int = 4 + 4 + int(b.k)*4 + int(b.m)
+func (b *BloomFilter) Serialize() []byte {
+
+	var size int = 4 + 4 + int(b.k)*32 + int(b.m)
 	bytes := make([]byte, size)
+
 	binary.BigEndian.PutUint32(bytes[0:4], uint32(b.m)) //Bitset length
 	binary.BigEndian.PutUint32(bytes[4:8], uint32(b.k)) //Number of hash functions
+
 	i := 0
 	for _, fn := range b.hashFunc {
-		copy(bytes[8+8*i:16+8*i], fn.Seed) //Hash seeds
+		copy(bytes[8+32*i:40+32*i], fn.Seed) //Hash seeds
 		i++
 	}
-	for i := 0; i < int(b.m); i++ {
-		bytes = append(bytes, b.bitset[i]) //Bitset
-	}
+
+	copy(bytes[size-int(b.m):], b.bitset) //bitset
+
 	return bytes
 }
+
 func Deserialize(bytes []byte) *BloomFilter {
 	b := &BloomFilter{}
+
 	m := uint(binary.BigEndian.Uint32(bytes[0:4]))
 	k := uint(binary.BigEndian.Uint32(bytes[4:8]))
+
 	hashFunc := make([]HashWithSeed, k)
 	for i := 0; i < int(k); i++ {
-		hashFunc[i] = HashWithSeed{Seed: bytes[8+i*8 : 16+8*i]}
+		hashFunc[i] = HashWithSeed{Seed: bytes[8+i*32 : 40+i*32]}
 	}
+
+	i := 0
+	for _, fn := range b.hashFunc {
+		copy(bytes[8+32*i:40+32*i], fn.Seed) //Hash seeds
+		i++
+	}
+
 	b.m = m
 	b.k = k
 	b.hashFunc = hashFunc
-	b.bitset = bytes[8+k*8:]
+	b.bitset = bytes[8+k*32:]
+
 	return b
 }
