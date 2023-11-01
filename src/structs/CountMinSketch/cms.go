623e5d161e919c9a4f9000e04f3ffda8cc710bf6
Milos <milicmilos.rs@gmail.com>
2023-11-01 10:11:32 +0100

    Added CountMinSketch Serialization/Deserialization

--- src/structs/CountMinSketch/cms.go
+++ src/structs/CountMinSketch/cms.go
@@ -1,6 +1,7 @@
 package countminsketch
 
 import (
+	"encoding/binary"
 	"math"
 )
 
@@ -45,4 +46,91 @@ func (cms *CMS) Search(new_element string) uint32 {
 		}
 	}
 	return minimum
+}
+
+// Returns a byte array representing a serialized CMS object
+func (cms *CMS) Serialize() []byte {
+	var size uint32 = 0
+	size += 4                 // 4 Bytes for K
+	size += 4                 // 4 Bytes for M
+	size += 4 * cms.k         // 4 Bytes K times because of K seeds
+	size += 4 * cms.k * cms.m // 4 Bytes per table field
+
+	data := make([]byte, size)
+
+	binary.BigEndian.PutUint32(data, cms.k)
+
+	m_slice := data[4:8]
+	binary.BigEndian.PutUint32(m_slice, cms.m)
+
+	hashes_slice_end := 8 + cms.k*4
+	hashes_slice := data[8:hashes_slice_end]
+
+	// Serialization of hash list
+	for i, hash := range cms.hashes {
+		seedBytes := hash.Seed
+		for j := 0; j < len(seedBytes); j++ {
+			hashes_slice[i*len(seedBytes)+j] = seedBytes[j]
+		}
+	}
+
+	// Serialization of table data
+	tableData := data[hashes_slice_end:]
+	for i := 0; i < int(cms.k); i++ {
+		for j := 0; j < int(cms.m); j++ {
+			value := cms.table[i][j]
+			offset := 4 * (i*int(cms.m) + j)
+			binary.BigEndian.PutUint32(tableData[offset:offset+4], value)
+		}
+	}
+
+	return data
+}
+
+// Creates and returns a new CMS object from serialized data
+func Deserialize(data []byte) *CMS {
+	cms := &CMS{}
+
+	K := binary.BigEndian.Uint32(data[0:4])
+	M := binary.BigEndian.Uint32(data[4:8])
+
+	hash_list := make([]HashWithSeed, K)
+
+	hashes_slice := data[8:]
+	// i is the number in order of the hash we are currently deserializing
+	for i := 0; i < int(K); i++ {
+		hash_slice := hashes_slice[i*4 : i*4+4]
+		seed_value := binary.BigEndian.Uint32(hash_slice) // Value of seed as a 32bit Integer
+
+		seed := make([]byte, 4) // Value of seed as an array of 4 bytes
+
+		binary.BigEndian.PutUint32(seed, seed_value) //Copy of seed value into the seed array
+		hash_list[i] = HashWithSeed{Seed: seed}
+	}
+
+	hashes_slice_end := 8 + K*4
+
+	// Allocation of table matrix
+	table := make([][]uint32, K)
+	for i := range table {
+		table[i] = make([]uint32, M)
+	}
+
+	// Deserialization of table
+	table_data_slice := data[hashes_slice_end:]
+	for i := uint32(0); i < K; i++ {
+		for j := uint32(0); j < M; j++ {
+			// Slice containing the data of one value from the table
+			value_index := (i*M + j) * 4 // i-th row, j-th column, and the size of each value is 4 bytes
+			value_slice := table_data_slice[value_index : value_index+4]
+			table[i][j] = binary.BigEndian.Uint32(value_slice)
+		}
+	}
+
+	cms.k = K
+	cms.m = M
+	cms.hashes = hash_list
+	cms.table = table
+
+	return cms
 }
