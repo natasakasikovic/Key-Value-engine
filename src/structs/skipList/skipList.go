b20727aac700a3130332efc3b6feffb54c626f96
Ana Šinik <sinikana7@gmail.com>
2023-11-11 18:51:24 +0100

    add find function

--- src/structs/skipList/skipList.go
+++ src/structs/skipList/skipList.go
@@ -1,7 +1,7 @@
 package skiplist
 
 import (
-	"fmt"
+	"errors"
 	"math/rand"
 	"strings"
 )
@@ -15,13 +15,11 @@ type node struct {
 	key   string
 	val   int
 	tower [maxHeight]*node // a collection of forwarded pointers linking the node to subsequent nodes on each corresponding level of the skip list
-
 }
 
 type SkipList struct {
-	maxHeight int   // the maximum number of levels
-	head      *node // starting head node
-	height    int   // total number of levels that all nodes are currently occupying
+	head   *node // starting head node
+	height int   // total number of levels that all nodes are currently occupying
 }
 
 func NewSkipList() *SkipList {
@@ -34,13 +32,15 @@ func NewSkipList() *SkipList {
 // search algoritham, returns requested node and track of the nodes that we pass through as we descend from level to level
 func (skipList *SkipList) search(key string) (*node, [maxHeight]*node) {
 	var next *node
-	var journey [maxHeight]*node
+	var journey [maxHeight]*node // store of the nodes along the search path, required for insertion and deletion
 
 	prev := skipList.head
+	// iteratie through the heights of the skip list, starting from the top and going down
 	for level := skipList.height - 1; level >= 0; level-- {
+		// iterate through nodes at the current level (level) from the current node (prev) towards the head node
 		for next = prev.tower[level]; next != nil; next = prev.tower[level] {
 			if strings.Compare(key, next.key) <= 0 {
-				break
+				break // move down to a lower level
 			}
 			prev = next
 		}
@@ -60,8 +60,15 @@ func (s *SkipList) Insert(key string, val int) {
 
 }
 
-func (s *SkipList) Find(key []byte) {
+// finding the precise value residing at a requested key; returns -1 if key not found and error
+func (skipList *SkipList) Find(key string) (int, error) {
+	found, _ := skipList.search(key)
 
+	if found == nil {
+		return -1, errors.New("key not found")
+	}
+
+	return found.val, nil
 }
 
 func (s *SkipList) roll() int {
@@ -69,16 +76,9 @@ func (s *SkipList) roll() int {
 	// possible ret values from rand are 0 and 1
 	// we stop shen we get a 0
 	for ; rand.Int31n(2) == 1; level++ {
-		if level >= s.maxHeight {
+		if level >= maxHeight {
 			return level
 		}
 	}
 	return level
-}
-
-func main() {
-	s := SkipList{maxHeight: 3}
-	for i := 0; i < 10; i++ {
-		fmt.Println(s.roll())
-	}
 }
