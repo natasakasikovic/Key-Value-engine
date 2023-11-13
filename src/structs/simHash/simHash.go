2e3e344e733f3e345bb41541851b8a61cceea2c2
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2023-11-13 14:15:10 +0100

    add getFingerprint function

--- src/structs/simHash/simHash.go
+++ src/structs/simHash/simHash.go
@@ -4,7 +4,35 @@ import (
 	"crypto/md5"
 	"encoding/binary"
 	"math/bits"
+	"strings"
 )
+
+func GetFingerprint(text string) uint64 {
+	words := filterWords(strings.Fields(text))
+	hashes := make(map[uint64]uint16) // key is hash, value is number of repetitions
+	for word, repeat := range words {
+		hashes[getWordHash(word)] = repeat
+	}
+
+	var fingerprint uint64 = 0
+	var mask uint64 = 1
+
+	for i := 0; i < 64; i++ {
+		sum := 0
+		for key, value := range hashes { // iterate over hashes, sum columns
+			if bits.OnesCount64(key&mask) == 1 {
+				sum += int(value)
+			} else {
+				sum -= int(value)
+			}
+		}
+		if sum > 0 { // if sum is greater than zero, put 1 in fingerprint
+			fingerprint |= mask
+		}
+		mask <<= 1 // shift mask
+	}
+	return fingerprint
+}
 
 // function removes stop words and gives weight if some word appeared more than once
 func filterWords(words []string) map[string]uint16 {
