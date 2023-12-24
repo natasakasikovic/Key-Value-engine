78268366ff8d659ee8d6da601797f785b2c3b0ed
draganjordanovic <dragan.jjj.03@gmail.com>
2023-12-24 13:12:27 +0100

    Add struct memtable

---
+++ src/structs/memtable/memtable.go
@@ -0,0 +1,25 @@
+package memtable
+
+type DataStructure interface {
+	Insert(key string, value []byte)
+	Delete(key string) //should be logical
+	IsFull(capacity uint64) bool
+	Find(key string) string //return value of the key
+}
+
+type Memtable struct {
+	data     DataStructure
+	capacity uint64
+	keys     []string
+}
+
+func NewMemtable(data DataStructure, capacity uint64) *Memtable {
+	if capacity == 0 {
+		capacity = 20
+	}
+
+	return &Memtable{
+		data:     data,
+		capacity: capacity,
+	}
+}
