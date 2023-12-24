e0494232ad9b29223da2a594aaf9fc3b84156d81
draganjordanovic <dragan.jjj.03@gmail.com>
2023-12-24 13:25:59 +0100

    Refactoring: Renamed function for consistency with the DataStructure interface

--- src/structs/B-Tree/btree.go
+++ src/structs/B-Tree/btree.go
@@ -91,7 +91,7 @@ func (btree *BTree) SearchRef(key string) *MemtableValue {
 
 //Search returns the value represented by the passed key, as well as a boolean indicating if the key exists.
 //If the key isn't found, the value returned will be an empty struct.
-func (btree *BTree) Search(key string) (MemtableValue, bool) {
+func (btree *BTree) Find(key string) (MemtableValue, bool) {
 	var ref *MemtableValue = btree.SearchRef(key)
 	if ref == nil {
 		return MemtableValue{}, false
