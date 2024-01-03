beb607e3033c36e8a9ecd7b431dc31f5232cd8b8
cofi420 <filip.vidakovic.04@gmail.com>
2024-01-03 16:08:26 +0100

    Fix lru cache Add UpdateKeys method

--- src/structs/LRUCache/LRUCache.go
+++ src/structs/LRUCache/LRUCache.go
@@ -1,6 +1,18 @@
 package LRUCache
 
-import "container/list"
+import (
+	"container/list"
+	"github.com/natasakasikovic/Key-Value-engine/src/structs/memtable"
+)
+
+type Data struct {
+	value []byte
+	key   string
+}
+
+func NewData(value []byte, key string) *Data {
+	return &Data{value: value, key: key}
+}
 
 type LRUCache struct {
 	hashMap    map[string]*list.Element
@@ -14,18 +26,34 @@ func NewLRUCache() *LRUCache {
 }
 
 func (lru *LRUCache) Add(key string, value []byte) {
-	lru.hashMap[key] = lru.list.PushFront(value)
+	data := NewData(value, key)
+	lru.hashMap[key] = lru.list.PushFront(data)
 	lru.numOfElems++
 	if lru.numOfElems > lru.limit {
+		//Delete from hashmap
+		delete(lru.hashMap, lru.list.Back().Value.(*Data).key)
+		//Delete from list
 		lru.list.Remove(lru.list.Back())
 	}
 }
 
-func (lru *LRUCache) Get(key string) any {
+func (lru *LRUCache) Get(key string) []byte {
 	elem, exists := lru.hashMap[key]
 	if !exists {
 		return nil
 	}
+	//Move to front of the list
 	lru.list.MoveToFront(elem)
-	return elem.Value
+	return elem.Value.(*Data).value
+}
+
+func (lru *LRUCache) UpdateKeys(mt *memtable.Memtable) {
+	for i := 0; i < len(mt.Keys); i++ {
+		elem, exists := lru.hashMap[mt.Keys[i]]
+		if exists {
+			record, _ := memtable.Get(mt.Keys[i])
+			value := record.Value
+			elem.Value.(*Data).value = value
+		}
+	}
 }
