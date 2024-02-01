8979a25f6de4a61a76fc21719fc88454e83696f3
Milos <milicmilos.rs@gmail.com>
2024-02-01 23:07:19 +0100

    SSTable iterators no longer depend on lsm tree

--- src/structs/iterators/sstable_iterator.go
+++ src/structs/iterators/sstable_iterator.go
@@ -1,15 +1,84 @@
 package iterators
 
 import (
+	"encoding/binary"
 	"fmt"
 	"io"
 	"os"
 
 	"github.com/natasakasikovic/Key-Value-engine/src/model"
-	lsmtree "github.com/natasakasikovic/Key-Value-engine/src/structs/LSMTree"
 	"github.com/natasakasikovic/Key-Value-engine/src/structs/sstable"
 	"github.com/natasakasikovic/Key-Value-engine/src/utils"
 )
+
+// deserializes a record by reading field by field from the file
+// returns Record, number of bytes read, error
+func Deserialize(file *os.File) (*model.Record, uint64, error) {
+	var err error
+	var record model.Record = model.Record{}
+
+	var keySizeBuffer []byte = make([]byte, 8)
+	_, err = io.ReadAtLeast(file, keySizeBuffer, 8)
+	if err != nil {
+		return nil, 0, err
+	}
+	record.KeySize = binary.BigEndian.Uint64(keySizeBuffer)
+
+	var bufferKey []byte = make([]byte, record.KeySize)
+	_, err = io.ReadAtLeast(file, bufferKey, int(record.KeySize))
+	if err != nil {
+		return nil, 0, err
+	}
+	record.Key = string(bufferKey)
+
+	var crcBuffer []byte = make([]byte, 4)
+	_, err = io.ReadAtLeast(file, crcBuffer, 4)
+	if err != nil {
+		return nil, 0, err
+	}
+	crc := binary.BigEndian.Uint32(crcBuffer)
+	record.Crc = crc
+
+	var timestampBuffer []byte = make([]byte, 8)
+	_, err = io.ReadAtLeast(file, timestampBuffer, 8)
+	if err != nil {
+		return nil, 0, err
+	}
+	timestamp := binary.BigEndian.Uint64(timestampBuffer)
+	record.Timestamp = timestamp
+
+	var tombstoneBuffer []byte = make([]byte, 1)
+	_, err = io.ReadAtLeast(file, tombstoneBuffer, 1)
+	if err != nil {
+		return nil, 0, err
+	}
+	tombstone := tombstoneBuffer[0]
+	record.Tombstone = byte(tombstone)
+
+	read := 8 + record.KeySize + 1 + 8 + 4
+	if tombstone != 1 {
+		var valueSizeBuffer []byte = make([]byte, 8)
+		_, err = io.ReadAtLeast(file, valueSizeBuffer, 8)
+		if err != nil {
+			return nil, 0, err
+		}
+		record.ValueSize = binary.BigEndian.Uint64(valueSizeBuffer)
+
+		var valueBuffer []byte = make([]byte, record.ValueSize)
+		_, err = io.ReadAtLeast(file, valueBuffer, int(record.ValueSize))
+
+		if err != nil {
+			return nil, 0, err
+		}
+		record.Value = valueBuffer
+		read += (8 + record.ValueSize)
+	} else {
+		record.ValueSize = 0
+		record.Value = []byte{}
+	}
+
+	return &record, read, nil
+}
 
 type SSTableIterator struct {
 	data           *os.File
@@ -84,7 +153,7 @@ func NewSSTableIterator(table *sstable.SSTable) (*SSTableIterator, error) {
 func (iter *SSTableIterator) Next() (*model.Record, error) {
 
 	for iter.current_offset < iter.end_offset {
-		record_p, _, err := lsmtree.Deserialize(iter.data)
+		record_p, _, err := Deserialize(iter.data)
 		if err != nil {
 			return nil, err
 		}
