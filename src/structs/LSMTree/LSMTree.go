322d74f731b458fbe5535b02d9c3dd09432ca505
Milos <milicmilos.rs@gmail.com>
2024-02-01 13:48:30 +0100

    Worked on merging sstables when compacting

--- src/structs/LSMTree/LSMTree.go
+++ src/structs/LSMTree/LSMTree.go
@@ -1,8 +1,22 @@
 package lsmtree
 
+/*
+STA MI TREBA:
+	-OBAVEZNO:
+	u konfig:
+		LSMFirstLevelSize (ceo broj)
+		LSMGrowthFactor (ceo broj)
+		LSMCompactionType (string "leveled" ili "sizetiered")
+	oni atributi za sstable da su javni
+	-Bilo bi jako lepo:
+		LoadSSTable metodica
+*/
+
 import (
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/natasakasikovic/Key-Value-engine/src/config"
@@ -10,6 +24,75 @@ import (
 	"github.com/natasakasikovic/Key-Value-engine/src/structs/sstable"
 	"github.com/natasakasikovic/Key-Value-engine/src/utils"
 )
+
+// deserializes a record by reading field by field from the file
+// returns Record, number of bytes read, error
+func Deserialize(file *os.File) (*model.Record, uint64, error) {
+	var err error
+	var record model.Record = model.Record{}
+
+	var keySizeBuffer []byte = make([]byte, 8)
+	_, err = io.ReadAtLeast(file, keySizeBuffer, 8)
+	if err != nil {
+		return nil, 0, err
+	}
+	record.KeySize = binary.BigEndian.Uint64(keySizeBuffer)
+
+	var bufferKey []byte = make([]byte, record.KeySize)
+	_, err = io.ReadAtLeast(file, bufferKey, int(record.KeySize))
+	if err != nil {
+		return nil, 0, err
+	}
+	record.Key = string(bufferKey)
+
+	var crcBuffer []byte = make([]byte, 4)
+	_, err = io.ReadAtLeast(file, crcBuffer, 4)
+	if err != nil {
+		return nil, 0, err
+	}
+	crc := binary.BigEndian.Uint32(crcBuffer)
+	record.Crc = crc
+
+	var timestampBuffer []byte = make([]byte, 8)
+	_, err = io.ReadAtLeast(file, timestampBuffer, 8)
+	if err != nil {
+		return nil, 0, err
+	}
+	timestamp := binary.BigEndian.Uint64(timestampBuffer)
+	record.Timestamp = timestamp
+
+	var tombstoneBuffer []byte = make([]byte, 1)
+	_, err = io.ReadAtLeast(file, tombstoneBuffer, 1)
+	if err != nil {
+		return nil, 0, err
+	}
+	tombstone := tombstoneBuffer[0]
+	record.Tombstone = byte(tombstone)
+
+	read := 8 + record.KeySize + 1 + 8 + 4
+	if tombstone != 1 {
+		var valueSizeBuffer []byte = make([]byte, 8)
+		_, err = io.ReadAtLeast(file, valueSizeBuffer, 8)
+		if err != nil {
+			return nil, 0, err
+		}
+		record.ValueSize = binary.BigEndian.Uint64(valueSizeBuffer)
+
+		var valueBuffer []byte = make([]byte, record.ValueSize)
+		_, err = io.ReadAtLeast(file, valueBuffer, int(record.ValueSize))
+
+		if err != nil {
+			return nil, 0, err
+		}
+		record.Value = valueBuffer
+		read += (8 + record.ValueSize)
+	} else {
+		record.ValueSize = 0
+		record.Value = []byte{}
+	}
+
+	return &record, read, nil
+}
 
 type LSMTree struct {
 	sstableArrays  [][]*sstable.SSTable //Array of arrays of SSTable pointers
@@ -32,10 +115,12 @@ func NewLSMTree(conf *config.Config) LSMTree {
 	return tree
 }
 
-func LoadLSMTreeFromFile(conf *config.Config, lsmFilename string) *LSMTree {
+const LSM_PATH string = "../data/LSMTree.json"
+
+func LoadLSMTreeFromFile(conf *config.Config) *LSMTree {
 	var lsm LSMTree = NewLSMTree(conf)
 
-	jsonData, err := os.ReadFile(lsmFilename)
+	jsonData, err := os.ReadFile(LSM_PATH)
 
 	if err != nil {
 		return nil
@@ -76,12 +161,11 @@ func LoadLSMTreeFromFile(conf *config.Config, lsmFilename string) *LSMTree {
 	return &lsm
 }
 
-func (tree *LSMTree) SaveToFile(lsmFilename string) error {
+func (tree *LSMTree) SaveToFile() error {
 	var filenames [][]string = make([][]string, tree.maxDepth)
 	for i := 0; i < int(tree.maxDepth); i++ {
 		for j := 0; j < len(tree.sstableArrays[i]); j++ {
-			//TODO: UPDATE WHEN NAME BECOMES PUBLIC!!!
-			//filenames[i] = append(filenames[i], tree.sstableArrays[i][j].Name)
+			filenames[i] = append(filenames[i], tree.sstableArrays[i][j].Name)
 		}
 	}
 
@@ -91,7 +175,7 @@ func (tree *LSMTree) SaveToFile(lsmFilename string) error {
 		return err
 	}
 
-	f, err := os.Create(lsmFilename)
+	f, err := os.Create(LSM_PATH)
 	if err != nil {
 		return err
 	}
@@ -108,31 +192,49 @@ func (tree *LSMTree) SaveToFile(lsmFilename string) error {
 // Merges the passed sstables and splits them into new sstables
 // Each new sstable contains at most maxSSTableElem elements
 // Merges into a single sstable if maxSSTableElem is 0
-func mergeSSTables(sstableArray []*sstable.SSTable) *sstable.SSTable {
+func mergeSSTables(sstableArray []*sstable.SSTable, conf config.Config) (*sstable.SSTable, error) {
 	var sstableCount int = len(sstableArray)
-	var indexes []int = make([]int, sstableCount)
 
-	var records []model.Record = make([]model.Record, sstableCount)
+	var records []*model.Record = make([]*model.Record, sstableCount)
+	var fileOffsets []int64 = make([]int64, sstableCount)
+	var fileOffsetLimits []int64 = make([]int64, sstableCount)
 
 	const EMPTY_KEY string = ""
 
-	//Get the keys of records at the current indexes of the sstables
-	var getKeys func() []string = func() []string {
-		var keys []string = make([]string, sstableCount)
-		for i := 0; i < sstableCount; i++ {
-			keys[i] = records[i].Key
+	//Initialize file iterators
+	for i := 0; i < sstableCount; i++ {
+		fileOffsets[i] = sstableArray[i].DataOffset
+
+		var err error
+		if sstableArray[i].DataOffset == 0 {
+			fileOffsetLimits[i], err = utils.GetFileLength(sstableArray[i].Data)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			fileOffsetLimits[i] = sstableArray[i].IndexOffset
 		}
-		return keys
+
+		sstableArray[i].Data.Seek(fileOffsets[i], io.SeekStart)
+
+		records[i], _, err = Deserialize(sstableArray[i].Data)
+		if err != nil {
+			return nil, err
+		}
+
+		fileOffsets[i], err = sstableArray[i].Data.Seek(0, io.SeekCurrent)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	//Get the smallest key of the keys at the current iterators of the sstables
 	var getMinKey func() string = func() string {
-		var keys []string = getKeys()
 		var minimum string = EMPTY_KEY
 
 		for i := 0; i < sstableCount; i++ {
-			if keys[i] != EMPTY_KEY && (keys[i] < minimum || minimum == EMPTY_KEY) {
-				minimum = keys[i]
+			if records[i].Key != EMPTY_KEY && (records[i].Key < minimum || minimum == EMPTY_KEY) {
+				minimum = records[i].Key
 			}
 		}
 
@@ -150,8 +252,27 @@ func mergeSSTables(sstableArray []*sstable.SSTable) *sstable.SSTable {
 		return count
 	}
 
+	var readNextRecord func(int) error = func(index int) error {
+		if fileOffsets[index] < fileOffsetLimits[index] {
+			var err error
+			records[index], _, err = Deserialize(sstableArray[index].Data)
+			if err != nil {
+				return err
+			}
+
+			fileOffsets[index], err = sstableArray[index].Data.Seek(0, io.SeekCurrent)
+			if err != nil {
+				return err
+			}
+		} else {
+			//If EOF, just give the record an empty key
+			records[index] = &model.Record{Key: EMPTY_KEY}
+		}
+		return nil
+	}
+
 	//Moves the indexes for the sstables containing duplicates of the minimum key forward by one
-	var moveDuplicatesForward func(string) = func(key string) {
+	var moveDuplicatesForward func(string) error = func(key string) error {
 		//Index of the most recent record with the key
 		var latestIndex int = -1
 		//Timestamp of the most recent record with the key
@@ -165,16 +286,21 @@ func mergeSSTables(sstableArray []*sstable.SSTable) *sstable.SSTable {
 		}
 
 		//Move all duplicates that arent the latest record with the key forward
-		//TODO: Just read the next record in the file
 		for i := 0; i < sstableCount; i++ {
 			if records[i].Key == key && i != latestIndex {
-				indexes[i]++
+				err := readNextRecord(i)
+				if err != nil {
+					return err
+				}
 			}
 		}
+
+		return nil
 	}
 
 	//Returns the index of the sstable iterator currently containing the passed key
 	//Assumes no other sstable iterator contains the key currently
+	//Only call after calling moveDuplicatesForward
 	var getIndexOfIteratorWithKey func(string) int = func(key string) int {
 		for i := 0; i < sstableCount; i++ {
 			if records[i].Key == key {
@@ -193,30 +319,74 @@ func mergeSSTables(sstableArray []*sstable.SSTable) *sstable.SSTable {
 		}
 
 		for getKeyCount(minKey) > 0 {
-			moveDuplicatesForward(minKey)
+			err := moveDuplicatesForward(minKey)
+			if err != nil {
+				return nil, err
+			}
 		}
 
 		var minIndex int = getIndexOfIteratorWithKey(minKey)
 		if records[minIndex].Tombstone == 0 {
-			var newRecord *model.Record = &model.Record{}
-			(*newRecord) = records[minIndex]
-			newRecords = append(newRecords, newRecord)
+			newRecords = append(newRecords, records[minIndex])
 		}
-		indexes[minIndex]++
+		err := readNextRecord(minIndex)
+		if err != nil {
+			return nil, err
+		}
 	}
 	//TODO
 	//Complete when config gets updated
-	//var newSSTable *sstable.SSTable = sstable.CreateSStable()
-	//return newSSTable
+	newSSTable, err := sstable.CreateSStable(newRecords, conf.SSTableInSameFile, conf.CompressionOn, int(conf.IndexSummaryDegree), int(conf.IndexSummaryDegree))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return newSSTable, nil
+}
+
+// Deletes the passed sstable from the disk and updates the name attribute of other sstables
+func (tree *LSMTree) deleteTable(table *sstable.SSTable) error {
+	//Key - old name, value - new name
+	//Will be used to update the name attribute of other sstables
+	var nameMapping map[string]string = make(map[string]string)
+	var deletedName = table.Name
+
+	dirContent, err := utils.GetDirContent(sstable.PATH) // dirContent - names of all sstables dirs
+	if err != nil {
+		return err
+	}
+
+	i := 0
+	for i < len(dirContent) {
+		if dirContent[i] == deletedName {
+			break
+		}
+		i++
+	}
+
+	for j := i; j < len(dirContent)-1; j++ {
+		nameMapping[dirContent[j]] = dirContent[j+1]
+	}
+
+	for i := 0; i < int(tree.maxDepth); i++ {
+		for j := 0; j < len(tree.sstableArrays[i]); j++ {
+			newName, exists := nameMapping[tree.sstableArrays[i][j].Name]
+			if exists {
+				tree.sstableArrays[i][j].Name = newName
+			}
+		}
+	}
+
+	table.Delete()
 	return nil
 }
 
-func (tree *LSMTree) leveledCompaction(levelIndex uint32) {
+func (tree *LSMTree) leveledCompaction(levelIndex uint32) error {
 	//The first table on the passed level will be merged with the appropriate tables of the next level
 	var upperTable *sstable.SSTable = tree.sstableArrays[levelIndex][0]
-	//TODO: UPDATE WHEN KEY GETTERS ARE ADDED
-	var minKey string = ""
-	var maxKey string = ""
+	var minKey string = upperTable.MinKey
+	var maxKey string = upperTable.MaxKey
 
 	//Index of the first table from the lower level that needs to be merged
 	//Index of the last table from the lower level that needs to be merged
@@ -225,8 +395,7 @@ func (tree *LSMTree) leveledCompaction(levelIndex uint32) {
 
 	//Find first table that needs to be merged
 	for i := 0; i < len(tree.sstableArrays[levelIndex+1]); i++ {
-		//TODO UPDATE WHEN SSTABLE KEYS BECOME PUBLIC!!!
-		var tableMaxKey string = ""
+		var tableMaxKey string = tree.sstableArrays[levelIndex+1][i].MaxKey
 		if minKey <= tableMaxKey {
 			leftIndex = i
 			break
@@ -235,8 +404,7 @@ func (tree *LSMTree) leveledCompaction(levelIndex uint32) {
 
 	//Find last table that needs to be merged
 	for i := 0; i < len(tree.sstableArrays[levelIndex+1]); i++ {
-		//TODO UPDATE WHEN KEYS BECOME PUBLIC!!!
-		var tableMinKey string = ""
+		var tableMinKey string = tree.sstableArrays[levelIndex+1][i].MinKey
 		if maxKey >= tableMinKey {
 			rightIndex = i
 		}
@@ -248,8 +416,7 @@ func (tree *LSMTree) leveledCompaction(levelIndex uint32) {
 		//Find the index of the first sstable with keys larger than the upper sstable
 		var firstLargerIndex int = -1
 		for i := 0; i < len(tree.sstableArrays[levelIndex+1]); i++ {
-			//TODO UPDATE WHEN KEYS BECOME PUBLIC!!!
-			var tableMinKey string = ""
+			var tableMinKey string = tree.sstableArrays[levelIndex+1][i].MinKey
 			if maxKey < tableMinKey {
 				firstLargerIndex = i
 				break
@@ -276,12 +443,16 @@ func (tree *LSMTree) leveledCompaction(levelIndex uint32) {
 			toMerge = append(toMerge, tree.sstableArrays[levelIndex+1][i])
 		}
 
-		var merged *sstable.SSTable = mergeSSTables(toMerge)
+		merged, err := mergeSSTables(toMerge, *tree.config)
+
+		if err != nil {
+			return err
+		}
 
 		//Delete sstables that were merged
-		upperTable.Delete()
+		tree.deleteTable(upperTable)
 		for i := leftIndex; i <= rightIndex; i++ {
-			tree.sstableArrays[levelIndex+1][i].Delete()
+			tree.deleteTable(tree.sstableArrays[levelIndex+1][i])
 		}
 
 		//Insert the merged sstable into the level and remove the deleted sstables from the level
@@ -303,9 +474,11 @@ func (tree *LSMTree) leveledCompaction(levelIndex uint32) {
 	levelLen := len(tree.sstableArrays[levelIndex])
 	tree.sstableArrays[levelIndex][levelLen-1] = nil
 	tree.sstableArrays[levelIndex] = tree.sstableArrays[levelIndex][:levelLen-1]
+
+	return nil
 }
 
-func (tree *LSMTree) sizeTieredCompaction(levelIndex uint32) {
+func (tree *LSMTree) sizeTieredCompaction(levelIndex uint32) error {
 	//Set all sstables on the passed level to be compacted
 	var toMerge []*sstable.SSTable = make([]*sstable.SSTable, 0)
 	for i := 0; i < len(tree.sstableArrays[levelIndex]); i++ {
@@ -313,20 +486,26 @@ func (tree *LSMTree) sizeTieredCompaction(levelIndex uint32) {
 	}
 
 	//Merge all sstables into a single new sstable
-	var merged *sstable.SSTable = mergeSSTables(toMerge)
+	merged, err := mergeSSTables(toMerge, *tree.config)
+
+	if err != nil {
+		return err
+	}
+
 	//Delete old sstables
 	for i := 0; i < len(tree.sstableArrays[levelIndex]); i++ {
-		tree.sstableArrays[levelIndex][i].Delete()
+		tree.deleteTable(tree.sstableArrays[levelIndex][i])
 	}
 	//Empty out the array for the compacted level
 	clear(tree.sstableArrays[levelIndex])
 	tree.sstableArrays[levelIndex] = tree.sstableArrays[levelIndex][:0]
 
 	tree.sstableArrays[levelIndex+1] = append(tree.sstableArrays[levelIndex+1], merged)
+	return nil
 }
 
 func (tree *LSMTree) compact(levelIndex uint32) {
-	if tree.compactionType == "LEVELED" {
+	if tree.compactionType == "leveled" {
 		tree.leveledCompaction(levelIndex)
 	} else {
 		tree.sizeTieredCompaction(levelIndex)
@@ -345,7 +524,7 @@ func uintPow(x, y uint32) uint32 {
 func (tree *LSMTree) getCapacityOfLevel(levelIndex uint32) uint32 {
 	//TODO: Update with config values
 	if tree.compactionType == "leveled" {
-		return uintPow(5, levelIndex+1)
+		return uintPow(5, levelIndex)
 	}
 	//TODO: DUMB MAGIC NUMBER
 	return 5
